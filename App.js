ac5cfd3dfaaf0ec25cb0dc29ee639fcb74370f83
BhatArj24 <56486136+BhatArj24@users.noreply.github.com>
2023-04-17 17:27:39 -0700

    m

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient'; // Import the LinearGradient component
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // Use LinearGradient as a wrapper for the View component to create a gray gradient background
+    <LinearGradient colors={['#e0e0e0', '#c0c0c0']} style={styles.gradient}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Allergy Sense</Text>
+        <StatusBar style="auto" />
+      </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  gradient: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
 });
