8e69e808b39029ce9918713102f8f32eedb2f5c5
BhatArj24 <56486136+BhatArj24@users.noreply.github.com>
2023-04-27 19:10:55 -0700

    mobile nav updated

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,16 +1,10 @@
-<<<<<<< Updated upstream
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import {Auth} from 'aws-amplify';
+import Menu from '../../images/Menu.png'
+import HomePageLogo from '../../images/HomePageLogo.png'
+import Camera from '../../images/Camera.png'
 
-=======
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-import {Auth} from 'aws-amplify';
-
-
-
->>>>>>> Stashed changes
 const HomeScreen = () => {
   const signOut = () => {
     Auth.signOut()
@@ -18,51 +12,66 @@ const HomeScreen = () => {
 
   return (
     <View>
-<<<<<<< Updated upstream
-=======
         <View style={styles.navbar}>
-
+          <Image source={Menu} style={styles.menu} />
+          <Image source={HomePageLogo} style={styles.homepagelogo}/>
+          <Image source={Camera} style={styles.camera}/>
         </View>
->>>>>>> Stashed changes
-      <Text style={{fontSize:24, alignSelf:"center", marginTop:50}}>Home, sweet home</Text>
-      <Text style={{width:"100%",textAlign:"center", color:"red",marginTop:50, fontSize:20}} onPress={signOut}>
+      <Text style={{width:"100%",textAlign:"center", color:"red",marginTop:100, fontSize:20}} onPress={signOut}>
         Sign Out
       </Text>
     </View>
-<<<<<<< Updated upstream
-  )
-}
-
-export default HomeScreen
-=======
     
   )
 }
 const styles = StyleSheet.create({
-    navbar: {
-        boxSizing: "border-box",
-    
+  navbar: {
+    boxSizing: "border-box",
     display: "flex",
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    paddingVertical: "12px",
-    paddingHorizontal: "20px",
-    gap:"40px",
-    
-    position:"absolute",
-    width: "388px",
-    height: "68px",
-    left: "0px",
-    top: "0px",
-    
-    /* Primary */
-    
-    background: "##6F2DBD",
-    border: "1px solid #000000"
-      }
-  });
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    gap: 28,
+    position: "absolute",
+    width: 395,
+    height: 115,
+    left: 0,
+    top: 0,
+    backgroundColor: "#6F2DBD",
+    borderWidth: 1,
+    borderColor: "#000000",
+  },
+  menu: {
+    flex: "none",
+    order: 0,
+    flexGrow: 0,
+    width: 35,
+    marginTop: 25,
+  },
+  homepagelogo: {
+    width: 214.47,
+    height: 43.6,
 
-export default HomeScreen
+    flex: "none",
+    order: 1,
+    flexGrow: 0,
+    marginTop: 25,
 
->>>>>>> Stashed changes
+  },
+  camera: {
+    width: 46,
+    height: 46,
+
+    flex: "none",
+    order: 2,
+    flexGrow: 0,
+    marginTop: 25,
+
+  },
+});
+
+
+export default HomeScreen;
+
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,5 +1,1 @@
-<<<<<<< Updated upstream
 export {default} from './HomeScreen';
-=======
-export {default} from './HomeScreen';
->>>>>>> Stashed changes
